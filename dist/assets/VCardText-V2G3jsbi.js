653e94e76d3b67859c737bf71e66d17cb33b904b
slimanfl <slimanite1996@gmail.com>
2024-10-22 06:19:55 -0700

    first commit

---
+++ dist/assets/VCardText-V2G3jsbi.js
@@ -0,0 +1,1 @@
+import{c as t}from"./VAvatar-l9WhXBoi.js";const e=t("v-card-text");export{e as V};
---
+++ dist/assets/VSpacer-DK0kFLDr.js
@@ -0,0 +1,1 @@
+/* empty css              */import{c as r}from"./VAvatar-l9WhXBoi.js";const e=r("v-spacer","div","VSpacer");export{e as V};
